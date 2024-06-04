193eaf45697ba7c2192c1b0a63ef14ea28e65399
vneet2057 <divin828@gmail.com>
2024-06-04 10:50:31 +0545

    push

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request,'dashboard/pages/products/index.html',{'products':products})
+
+def addProduct(request):
+    return render(request,'dashboard/pages/products/add.html')
--- project/urls.py
+++ project/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('',index),
     path('dashboard/',dashboard),
     path('customer/',include('customer.urls')),
+    path('product/',include('product.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
