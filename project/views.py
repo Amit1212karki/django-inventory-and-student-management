f29746814141257f9f286b3b7388c32000e9d424
vneet2057 <divin828@gmail.com>
2024-06-17 07:56:09 +0545

    push

--- project/views.py
+++ project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from sales.models import *
+from django.db.models import Sum
 
 
 def index(request):
@@ -19,4 +21,20 @@ def index(request):
         return render(request, 'auth/login.html')
 @login_required
 def dashboard(request):
-    return render(request,'dashboard/pages/index.html')
+    total_sales = Sales.objects.aggregate(total=Sum('total'))['total'] or 0
+    total_transaction = Transaction.objects.aggregate(total=Sum('amount'))['total'] or 0
+
+
+    total_sales = round(total_sales, 2)
+    total_transaction = round(total_transaction, 2)
+    total_pending_amount  = total_sales - total_transaction
+
+
+    context = {
+    'total_sales': total_sales,
+    'total_transaction':total_transaction,
+    'total_pending_amount':total_pending_amount
+
+    }
+
+    return render(request,'dashboard/pages/index.html',context)
