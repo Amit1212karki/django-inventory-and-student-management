fbb5d5922cca3d0b0b112b0ff8559a5c5442afb1
vneet2057 <divin828@gmail.com>
2024-06-17 02:27:53 +0000

    push

--- project/settings.py
+++ project/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- project/urls.py
+++ project/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     path('sales/',include('sales.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
