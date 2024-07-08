0eb92f2ed940674fe3133a9ce24f703892e03a9d
vneet2057 <divin828@gmail.com>
2024-07-08 20:23:44 +0545

    push

--- project/settings.py
+++ project/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.hunchhadigital.com.np'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'billings@hunchhadigital.com.np'  
 EMAIL_HOST_PASSWORD = 'Ultimate@22'  
